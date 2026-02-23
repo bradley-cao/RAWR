9798aff3ff6cb7efd563e3d0ba7ba9fd4ea8e5e0
Bradley Cao <bradleycao@gmail.com>
2026-02-23 18:23:41 -0500

    basic .tiff/.nef byte-by-byte reading

---
+++ read_raw.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ifstream file("image.tiff", std::ios::binary);
+
+    if (!file) {
+        std::cerr << "Could not open file\n";
+        return 1;
+    }
+
+    char buffer[4096];
+
+    while (file.read(buffer, sizeof(buffer))) {
+        std::streamsize count = file.gcount();
+        for (std::streamsize i = 0; i < count; ++i) {
+            unsigned char byte = buffer[i];
+            std::cout << std::hex << (int)byte << " ";
+        }
+    }
+
+    // Handle last partial read
+    std::streamsize count = file.gcount();
+    for (std::streamsize i = 0; i < count; ++i) {
+        unsigned char byte = buffer[i];
+        std::cout << std::hex << (int)byte << " ";
+    }
+
+    file.close();
+    return 0;
+}
